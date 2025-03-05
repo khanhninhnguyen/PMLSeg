f6a7766911394af89fe8092ae42125f4fc1d7d44
Olivier BOCK <54418996+olivierbock@users.noreply.github.com>
2025-03-05 12:48:03 +0100

    Update Segmentation.R

--- R/Segmentation.R
+++ R/Segmentation.R
@@ -1,6 +1,6 @@
 #' Segmentation of time series by Penalized Maximum Likelihood
 #'
-#' Fit a segmentation in the mean model by taking into account a functional part (Fourier series of order 4) and a variance chaing over fixed intervals (monthly)
+#' Fit a segmentation in the mean model by taking into account a functional part (Fourier series of order 4) and a variance changing over fixed intervals (monthly)
 #'
 #' @param OneSeries is a time series data frame with 2 columns, $signal and $date, each of size n x 1.
 #' @param lmin is the minimum length of the segments. Default value is 1.
