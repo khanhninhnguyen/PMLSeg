2e31ab5ab020a98e4979bec15311d96216d27982
Olivier Bock <bock@ipgp.fr>
2025-05-05 15:16:21 +0200

    add update PlotSeg

--- R/PlotSeg.R
+++ R/PlotSeg.R
@@ -27,8 +27,6 @@ PlotSeg <- function(OneSeries,
                     labely = "signal") {
 
 
-  x_period = "1 year"
-
   MaxPoint = max(OneSeries$signal, na.rm = TRUE) + 0.5
   MinPoint = min(OneSeries$signal, na.rm = TRUE) - max(max(sqrt(SegRes$MonthVar)), max(SegRes$FitF, na.rm = TRUE)) - 0.5
 
@@ -43,35 +41,40 @@ PlotSeg <- function(OneSeries,
 
   colors <- c("signal" = "gray", "Mean" = "red", "FitF" = "purple", "MonthStd" = "cyan")
 
+  # mutate OneSeries
   OneSeries <- OneSeries %>%
     mutate(Mean = rep(SegRes$Tmu$mean, times = (SegRes$Tmu$end - SegRes$Tmu$begin + 1)),
            Month = as.numeric(format(date, "%m")),
-           MonthStd = sqrt(SegRes$MonthVar[as.numeric(Month)]) - abs(MinPoint)) %>%
+           MonthStd = sqrt(SegRes$MonthVar[as.numeric(Month)]) - abs(MinPoint)) %>%  
     select(-Month)
-
   if (FunctPart==TRUE){
     OneSeries <- OneSeries %>%
       mutate( FitF = SegRes$FitF - abs(MinPoint))
   }
 
+  # dates of CPs
+  SegRes$Tmu <- SegRes$Tmu %>%
+    mutate(tbegin = OneSeries$date[SegRes$Tmu$begin], 
+           tend = OneSeries$date[SegRes$Tmu$end])
 
-  # validated CP
+  # validated CPs
   if (!is.null(Validated_CP_Meta)) {
     validCP = Validated_CP_Meta$CP[which(Validated_CP_Meta$valid == 1)]
     OneSeries <- OneSeries %>%
       mutate(valid = ifelse(date %in% validCP, MinPoint, NA))
-    shapes <- c(shapes, "valid" = 17)
+    # shape for validated CP=0 (0=open square)
+    shapes <- c(shapes, "valid" = 0)
   }
 
-  # Remove cluster
+  # Replace data in clusters by NA 
+  # in signal
   if (!is.null(RemoveData)) {
-    RemoveInd <- unlist(lapply(1:nrow(RemoveData), function(x) {
+    index <- unlist(lapply(1:nrow(RemoveData), function(x) {
       RemoveData$begin[x]:RemoveData$end[x]
     }))
-
-    OneSeries[RemoveInd, c("signal", "Mean")] <- NA
+    OneSeries[index, c("signal")] <- NA
   }
-
+  # in other variables
   is_na_signal <- is.na(OneSeries$signal)
   if(sum(is_na_signal) > 0){
     if (FunctPart) {
@@ -79,7 +82,6 @@ PlotSeg <- function(OneSeries,
     } else {
       cols_to_update <- c("MonthStd", "Mean")
     }
-
     if(all(cols_to_update %in% names(OneSeries))) {
       OneSeries[is_na_signal, cols_to_update] <- NA
     } else {
@@ -87,7 +89,7 @@ PlotSeg <- function(OneSeries,
     }
   }
 
-  # Expand the series to be continuous
+  # Expand the series to be continuous (missing dates are replaced by NA in all variables)
   OneSeries <- OneSeries %>%
     tidyr::complete(date = seq(min(date), max(date), by = "day"))
 
@@ -97,6 +99,7 @@ PlotSeg <- function(OneSeries,
     types = unique(Metadata$type)
     OneSeries[types] <- NA
 
+    # shapes for metadata = 1:n (1=open circle, 2=open triangle...)
     MetaShape = stats::setNames(c(1:length(types)), types)
     shapes <- c(shapes, MetaShape)
 
@@ -110,10 +113,9 @@ PlotSeg <- function(OneSeries,
       dplyr::mutate( Meta = ifelse(date %in% Metadata, MaxPoint, NA))
     shapes <- c(shapes, "Meta" = 1)
   }
+  
   # make data for plot
-
   variable_names <- names(OneSeries)[-1]  # Exclude the first column which is 'date'
-
   long_data <- OneSeries %>%
     tidyr::pivot_longer(
       cols = all_of(variable_names), # Specify the columns to pivot
@@ -123,6 +125,7 @@ PlotSeg <- function(OneSeries,
     )
   long_data$variable = factor(long_data$variable, levels = variable_names)
 
+  # plot the signal, Mean, MonthStd + add zero line for signal (y=0) and MonthStd (y=MinPoint)
   p <- ggplot2::ggplot(long_data, aes(x = date, y = value, colour = variable, shape = variable)) +
     theme_bw() +
     geom_line(data = subset(long_data, variable %in% c("signal")),
@@ -132,42 +135,41 @@ PlotSeg <- function(OneSeries,
     geom_hline(yintercept = 0, size = 0.3, lty = 1, color = "black",na.rm = TRUE) +
     geom_hline(yintercept = MinPoint, size = 0.3, lty = 1, color = "black",na.rm = TRUE)
 
-
+  # plot functional
   if (FunctPart==TRUE){
     p <- p + geom_line(data=subset(long_data, variable == "FitF"), size = 0.5, na.rm = TRUE)
   }
 
-  # Add changepoints detected by segmentation
+  # Add vertical lines for CPs detected by segmentation
   if (nrow(SegRes$Tmu) > 1) {
     p <- p +
-      geom_vline(xintercept = OneSeries$date[SegRes$Tmu$end[-nrow(SegRes$Tmu)]],
-                 size = 0.5, lty = 2, color = "black", alpha = 0.6,na.rm = TRUE)
+      geom_vline(xintercept = SegRes$Tmu$tend[-nrow(SegRes$Tmu)],
+                 size = 0.5, lty = 2, color = "red", alpha = 0.6,na.rm = TRUE)
   }
 
+  # Add marker of validated CPs
   if (!is.null(Validated_CP_Meta)) {
     p <- p +
-      geom_point(data = subset(long_data, variable == "valid"), color = "coral", aes(shape = variable),
+      geom_point(data = subset(long_data, variable == "valid"), color = "red", aes(shape = variable),
                  size = 2.5,na.rm = TRUE)
   }
 
+  # Add markers of Metadata
   if (!is.null(Metadata)) {
     if (!is.null(Metadata$type)) {
       p <- p +
-        geom_point(data = subset(long_data, variable %in% types), color = "coral", aes(shape = variable),
+        geom_point(data = subset(long_data, variable %in% types), aes(shape = variable, color = variable),
                    size = 2, na.rm = TRUE)
     } else {
       p <- p +
-        geom_point(data = subset(long_data, variable == "Meta"), color = "coral",  aes(shape = variable),
+        geom_point(data = subset(long_data, variable == "Meta"), color = "blue",  aes(shape = variable),
                    size = 2, na.rm = TRUE)
     }
   }
 
-  if (!is.null(Metadata)){
-    p <- p +
-      scale_x_date(
-        limits = as.Date(c(OneSeries$date[1], OneSeries$date[nrow(OneSeries)])),
-        breaks = function(x) seq.Date(from = OneSeries$date[1], to = OneSeries$date[nrow(OneSeries)], by = x_period),
-        date_labels = "%Y") +
+  # Add labels
+  p <- p + 
+      scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
       scale_color_manual(values = colors) +
       scale_shape_manual(values = shapes) +
       labs(x = labelx, y = labely, color = "", fill = "") +
@@ -177,21 +179,37 @@ PlotSeg <- function(OneSeries,
         panel.grid.minor = element_blank(),
         panel.background = element_blank()
       )
-  } else {
-    p <- p +
-      scale_x_date(
-        limits = as.Date(c(OneSeries$date[1], OneSeries$date[nrow(OneSeries)])),
-        breaks = function(x) seq.Date(from = OneSeries$date[1], to = OneSeries$date[nrow(OneSeries)], by = x_period),
-        date_labels = "%Y") +
-      scale_color_manual(values = colors) +
-      labs(x = labelx, y = labely, color = "", fill = "") +
-      theme(
-        legend.title = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank()
-      )
-  }
+
+  # if (!is.null(Metadata)){
+    # p <- p +
+      # scale_x_date(
+        # limits = as.Date(c(OneSeries$date[1], OneSeries$date[nrow(OneSeries)])),
+        # breaks = function(x) seq.Date(from = OneSeries$date[1], to = OneSeries$date[nrow(OneSeries)], by = x_period),
+        # date_labels = "%Y") +
+      # scale_color_manual(values = colors) +
+      # scale_shape_manual(values = shapes) +
+      # labs(x = labelx, y = labely, color = "", fill = "") +
+      # theme(
+        # legend.title = element_blank(),
+        # panel.grid.major = element_blank(),
+        # panel.grid.minor = element_blank(),
+        # panel.background = element_blank()
+      # )
+  # } else {
+    # p <- p +
+      # scale_x_date(
+        # limits = as.Date(c(OneSeries$date[1], OneSeries$date[nrow(OneSeries)])),
+        # breaks = function(x) seq.Date(from = OneSeries$date[1], to = OneSeries$date[nrow(OneSeries)], by = x_period),
+        # date_labels = "%Y") +
+      # scale_color_manual(values = colors) +
+      # labs(x = labelx, y = labely, color = "", fill = "") +
+      # theme(
+        # legend.title = element_blank(),
+        # panel.grid.major = element_blank(),
+        # panel.grid.minor = element_blank(),
+        # panel.background = element_blank()
+      # )
+  # }
 
   p
   return(p)
